7e432bfb1a3ea5d26cae60f18d904cf6343f9bb1
rifat3568 <mazharulislam3569@gmail.com>
2023-10-20 12:05:15 +0600

    navbar added with 3 routes

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTvQrYyB1u2txeOvH5ejUbwGlNxHA0fMQ",
+  authDomain: "assingment-09-e6997.firebaseapp.com",
+  projectId: "assingment-09-e6997",
+  storageBucket: "assingment-09-e6997.appspot.com",
+  messagingSenderId: "523472144144",
+  appId: "1:523472144144:web:c539974722e043c8048795"
+};
+
+// Initialize Firebase
+const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+export default auth;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,10 @@
+
+const Register = () => {
+    return (
+        <div>
+            <h3 className="text-3xl">This is Regiser page</h3>
+        </div>
+    );
+};
+
+export default Register;
